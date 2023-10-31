2f1eeb90446451d008ea87fd5f3601bf3e5bfd56
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2023-10-31 22:45:35 +0530

    🚧 Intial Commit

--- login.php
+++ login.php
@@ -1,21 +1,34 @@
 <?php include_once 'partials/header.php'; ?>
-<title>Time Table</title>
+<title>Login Page</title>
 </head>
 <body>
 <?php include_once 'partials/navbar.php';?>
   <div class="container">
-    <div class="sub-container">
-      <h2>LOGIN</h2>
-      <form action="login.php">
-        <input type="text" name="email" placeholder="Email Address">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Login">
-      </form>
-      <p>Don't have an account? <a href="register.html">Register</a></p>
-    </div>
-    <img src="./images/login,register/bus-track.jpg" alt="bus-track">
+
+  <h1 class="text-primary">Login</h1>
+   
+  <div class="row">
+    <div class="col-md-6"><form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
-  <script src="./scripts/nav-responsive.js"></script>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+    <div id="pwdHelp" class="form-text">Your Password is protected with me</div>
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form></div>
+    <div class="col-md-6"></div>
+  </div>
+
+
 </body>
 
 </html>
