c56d13e14563e36d35b72799bd012e1663d9d2a2
Asela <43272744+zaselalk@users.noreply.github.com>
2023-11-05 01:39:57 +0530

    :bug: Implemented error handling in conn

--- conn.php
+++ conn.php
@@ -1,7 +1,13 @@
 <?php
-
 $dbHost = 'localhost';
 $dbUser = 'root';
 $dbPass = '';
 
-$connect = new PDO("mysql:host=localhost;dbname=bus_timer", $dbUser, $dbPass);
+try {
+    $conn = new PDO("mysql:host=$dbHost;dbname=bus_timer", $dbUser, $dbPass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; // You can replace this with any action you want upon successful connection
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
