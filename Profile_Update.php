724b7f68f65f2f7d691e292aef937c2a5e8c5269
Anjana Kavidu <anjanakavid@gmail.com>
2023-11-05 22:22:21 +0530

    Update user profile page with improved functionality and styling

--- Profile_Update.php
+++ Profile_Update.php
@@ -1,47 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
-
+    <title>Update User Profile</title>
+    <?php include_once 'partials/header.php'; ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
+<?php include_once 'partials/navbar.php'; ?>
     <div class="container">
         <h1 class="text-primary">User Profile</h1>
-
         <?php
+        include_once 'conn.php';
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $newName = $_POST['name'];
             $newEmail = $_POST['email'];
             $newDateOfBirth = $_POST['date_of_birth'];
             $newPhoneNumber = $_POST['phone_number'];
 
-            $servername = "localhost";
-            $username = "root";
-            $password = "root1234";
-            $database = "bus_timer";
-
-            $conn = mysqli_connect($servername, $username, $password, $database);
-
-            if (!$conn) {
-                die("Connection failed: " . mysqli_connect_error());
-            }
-
-            $user_id = 8; // Replace with the actual user ID
+            $user_id = $_SESSION['user']['user_id'];
             $sql = "UPDATE users SET name=?, email=?, date_of_birth=?, phone_number=? WHERE user_id=?";
-            $stmt = mysqli_prepare($conn, $sql);
-            mysqli_stmt_bind_param($stmt, "ssssi", $newName, $newEmail, $newDateOfBirth, $newPhoneNumber, $user_id);
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$newName, $newEmail, $newDateOfBirth, $newPhoneNumber, $user_id]);
 
-            if (mysqli_stmt_execute($stmt)) {
-                echo '<p class="alert alert-success">Profile updated successfully!</p>';
-            } else {
-                echo '<p class="alert alert-danger">Profile update failed.</p>';
-            }
-
-            mysqli_stmt_close($stmt);
-            mysqli_close($conn);
+            echo '<p class="alert alert-success">Profile updated successfully!</p>';
         }
         ?>
 
@@ -86,31 +71,9 @@
             if (move_uploaded_file($_FILES["profile_picture"]["tmp_name"], $uploadPath)) {
                 echo "Profile picture uploaded successfully. The file is stored as: $uploadPath";
 
-                $servername = "localhost";
-                $username = "root";
-                $password = "root1234";
-                $database = "bus_timer";
-
-                $conn = mysqli_connect($servername, $username, $password, $database);
-
-                if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
-
-                $user_id = 8;
-
                 $sql = "UPDATE users SET user_profile_pic = ? WHERE user_id = ?";
-                $stmt = mysqli_prepare($conn, $sql);
-                mysqli_stmt_bind_param($stmt, "si", $uploadPath, $user_id);
-
-                if (mysqli_stmt_execute($stmt)) {
-                    echo "Profile picture location stored in the database.";
-                } else {
-                    echo "Error updating the profile picture location in the database: " . mysqli_error($conn);
-                }
-
-                mysqli_stmt_close($stmt);
-                mysqli_close($conn);
+                $stmt = $conn->prepare($sql);
+                $stmt->execute([$uploadPath, $user_id]);
             } else {
                 echo "Error uploading the profile picture.";
             }
@@ -124,4 +87,5 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
