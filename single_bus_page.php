49c4a2ae479853fb3d03970eeb31abf0fb247598
Sandali3000 <127072941+Sandali3000@users.noreply.github.com>
2023-11-05 22:13:25 +0530

    update

--- single_bus_page.php
+++ single_bus_page.php
@@ -18,6 +18,43 @@ if (isset($_GET['id'])) {
         die("Connection failed: " . $conn->connect_error);
     }
 
+            // SQL to create table
+$sql_create_table = "CREATE TABLE buses (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  bus_name VARCHAR(255),
+  bus_number VARCHAR(255),
+  capacity INT,
+  last_maintenance_date DATE,
+  start_from VARCHAR(255),
+  end_at VARCHAR(255),
+  bus_stops VARCHAR(255),
+  departure_time TIME
+)";
+
+if ($conn->query($sql_create_table) === TRUE) {
+echo "Table buses created successfully";
+} else {
+echo "Error creating table: " . $conn->error;
+}
+
+// SQL to insert data
+$sql_insert_data = "INSERT INTO buses (bus_name, bus_number, capacity, last_maintenance_date, start_from, end_at, bus_stops, departure_time) VALUES
+('Red Arrow', 'Bus-1001', 50, '2023-10-15', 'New York', 'Boston', 'Albany, Hartford, Worcester', '08:00:00'),
+('Blue Lightning', 'Bus-1002', 45, '2023-09-21', 'Chicago', 'Miami', 'Indianapolis, Nashville, Atlanta', '09:30:00'),
+('Green Cruiser', 'Bus-1003', 60, '2023-08-18', 'San Francisco', 'Seattle', 'Sacramento, Portland', '11:45:00'),
+('Yellow Rider', 'Bus-1004', 55, '2023-07-25', 'Houston', 'Dallas', 'Austin, Waco', '10:00:00'),
+('Silver Express', 'Bus-1005', 40, '2023-06-30', 'Atlanta', 'Washington', 'Charlotte, Richmond, Baltimore', '07:15:00'),
+('Orange Voyager', 'Bus-1006', 65, '2023-05-12', 'Denver', 'Phoenix', 'Colorado Springs, Albuquerque', '12:00:00'),
+('Purple Traveler', 'Bus-1007', 50, '2023-04-02', 'Orlando', 'Tampa', 'Lakeland', '11:30:00'),
+('Black Navigator', 'Bus-1008', 55, '2023-03-14', 'Las Vegas', 'San Diego', 'Los Angeles, Palm Springs', '09:15:00'),
+('White Wayfarer', 'Bus-1009', 60, '2023-02-07', 'Portland', 'Vancouver', 'Seattle', '10:45:00'),
+('Gold Adventurer', 'Bus-1010', 45, '2023-01-19', 'Boston', 'New York', 'Worcester, Hartford, Albany', '08:45:00')";
+
+if ($conn->query($sql_insert_data) === TRUE) {
+echo "New records created successfully";
+} else {
+echo "Error: " . $sql_insert_data . "<br>" . $conn->error;
+}
 
 
     $id = $_GET['id'];
