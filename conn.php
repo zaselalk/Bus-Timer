a7c8ad8c79595b51baf532bdcb27d19fda08e8ac
Asela <43272744+zaselalk@users.noreply.github.com>
2023-11-05 10:47:11 +0530

    :card_file_box: Update database connection

--- conn.php
+++ conn.php
@@ -6,7 +6,7 @@ $dbPass = '';
 try {
     $conn = new PDO("mysql:host=$dbHost;dbname=bus_timer", $dbUser, $dbPass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; // You can replace this with any action you want upon successful connection
+   
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
