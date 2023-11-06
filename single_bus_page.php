3a1e28f1c4c1a7f896e7a8b3ffc0048341890df9
Sandali3000 <127072941+Sandali3000@users.noreply.github.com>
2023-11-06 05:53:12 +0530

    update

--- single_bus_page.php
+++ single_bus_page.php
@@ -6,52 +6,49 @@
 
 <?php
 $servername = "localhost";
-$username = "root"; 
-$password = "root1234"; 
+$username = "root";
+$password = "root1234";
 $dbname = "bus_route";
 
 if (isset($_GET['id'])) {
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-  
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-            // SQL to create table
-$sql_create_table = "CREATE TABLE buses (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  bus_name VARCHAR(255),
-  bus_no VARCHAR(255),
-  capacity INT,
-  last_maintenance_date DATE,
-  start_from VARCHAR(255),
-  end_at VARCHAR(255),
-  bus_stops VARCHAR(255),
-  departure_time TIME
-)";
+    // SQL to create table
+    $sql_create_table = "CREATE TABLE IF NOT EXISTS buses (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        bus_name VARCHAR(255),
+        bus_no VARCHAR(255),
+        capacity INT,
+        last_maintenance_date DATE,
+        start_from VARCHAR(255),
+        end_at VARCHAR(255),
+        bus_stops VARCHAR(255),
+        departure_time TIME
+    )";
 
-if ($conn->query($sql_create_table) === TRUE) {
-echo "Table buses created successfully";
-} else {
-echo "Error creating table: " . $conn->error;
-}
+    if ($conn->query($sql_create_table) === TRUE) {
+        echo "Table buses created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
 
-// SQL to insert data
-$sql_insert_data = "INSERT INTO buses (id,bus_name, bus_no, capacity, last_maintenance_date, start_from, end_at, bus_stops, departure_time) VALUES
-(1,'City Express', 'AB1234', 50, '2023-10-15', 'Jaffna', 'Colombo', 'Kandy, Gampaha, BAtticaloa', '08:00:00'),
-(2,'Metro Cruiser', 'BX2343', 45, '2023-09-21', 'Polonnaruwa', 'Kandy', 'Kurunegala, Kegalle, Anuradhapura', '09:30:00'),
-(3,'Sunset Shuttle', 'LM3432', 60, '2023-08-18', 'Kurunegala', 'Colombo', 'Gampaha, Migamuwa', '11:45:00'),
-(4,'Green Transit', 'LC4523', 55, '2023-07-25', 'Batticaloa', 'Matara', 'Katharagama, Colombo', '10:00:00'),
-(5,'Skyliner Coach', 'LK4532', 40, '2023-06-30', 'Colombo', 'Kandy', 'Mirigama, Kegalle, Gampaha', '07:15:00'),
+    // SQL to insert data
+    $sql_insert_data = "INSERT INTO buses (bus_name, bus_no, capacity, last_maintenance_date, start_from, end_at, bus_stops, departure_time) VALUES
+        ('City Express', 'AB1234', 50, '2023-10-15', 'Jaffna', 'Colombo', 'Kandy, Gampaha, BAtticaloa', '08:00:00'),
+        ('Metro Cruiser', 'BX2343', 45, '2023-09-21', 'Polonnaruwa', 'Kandy', 'Kurunegala, Kegalle, Anuradhapura', '09:30:00'),
+        ('Sunset Shuttle', 'LM3432', 60, '2023-08-18', 'Kurunegala', 'Colombo', 'Gampaha, Migamuwa', '11:45:00'),
+        ('Green Transit', 'LC4523', 55, '2023-07-25', 'Batticaloa', 'Matara', 'Katharagama, Colombo', '10:00:00'),
+        ('Skyliner Coach', 'LK4532', 40, '2023-06-30', 'Colombo', 'Kandy', 'Mirigama, Kegalle, Gampaha', '07:15:00')";
 
-
-if ($conn->query($sql_insert_data) === TRUE) {
-echo "New records created successfully";
-} else {
-echo "Error: " . $sql_insert_data . "<br>" . $conn->error;
-}
-
+    if ($conn->query($sql_insert_data) === TRUE) {
+        echo "New records created successfully";
+    } else {
+        echo "Error: " . $sql_insert_data . "<br>" . $conn->error;
+    }
 
     $id = $_GET['id'];
     $sql = "SELECT * FROM buses WHERE bus_name = '$id'";
@@ -67,10 +64,9 @@ echo "Error: " . $sql_insert_data . "<br>" . $conn->error;
             echo "Capacity: " . $row["capacity"] . "<br>";
             echo "Last Maintenance Date: " . $row["last_maintenance_date"] . "<br>";
             echo "Departure time: " . $row["departure_time"] . "</br>";
-            echo "start from: " . $row["start_from"] . "</br>";
-            echo "end at : " . $row["end_at"] . "</br>";
+            echo "Start From: " . $row["start_from"] . "</br>";
+            echo "End At: " . $row["end_at"] . "</br>";
             echo "Bus Stops: " . $row["bus_stops"] . "</br>";
-            
         } else {
             echo "No bus found with that name.";
         }
